2143f0265bec922b3c372a84978f6e9162784e7c
lolomari <isepribo@gmail.com>
2022-09-14 16:40:00 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * time_table - function that prints time table for 9
+ * times_table - function that prints time table for 9
  *
  */
 
-void time_table(void)
+void times_table(void)
 {
 	int x = 0; /* factor */
 	int y; /* count */
