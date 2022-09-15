43b6d59298860680dde58a26ffd0babaf4646af6
lolomari <isepribo@gmail.com>
2022-09-15 14:23:35 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draw a diagonal line
+ * @n: The lenght of the line
+ */
+
+void print_diagonal(int n)
+{
+	int column, space, i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (column = 0; column < n; column++)
+		{
+			i = column - 1;
+
+			for (space = 0; space <= 1; space++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
