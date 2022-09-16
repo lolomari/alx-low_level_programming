52343c5ed3bdbf78ecbb90df3192008104fd1769
lolomari <isepribo@gmail.com>
2022-09-16 07:51:43 -0700

    0-main.c

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive-or_negative(int i);
+int latgest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int years)
+int convert_day(int month, int day);
+
+#endif /* MAIN_H */
