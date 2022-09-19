1cf02566edf18cedc624f81aae22731dadcb1d68
lolomari <isepribo@gmail.com>
2022-09-19 15:45:33 +0100

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -8,12 +8,13 @@
 void reset_to_98(int *n)
 
 {
-	int n;
+	n = 98;
 
-	n = 402;
-	printf("%d\n", n);
-	reset_to_98(&n);
-
+	/**
+	 * n = 402;
+	 * printf("%d\n", n);
+	 * reset_to_98(&n);
+	 */
 
 	return (0)
 }
