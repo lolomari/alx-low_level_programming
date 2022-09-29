c69daebbb73974ef0b9f8b6a01e28ce7df222737
lolomari <isepribo@gmail.com>
2022-09-29 10:57:48 +0100

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,45 +1,36 @@
+#include "main.h"
+
 /**
- * _sqrt_recursion_wrapper - a wrapper that does the recursion bit
+ * _sqrt_recursion - returns the natural square root of a number
  *
- * @n : input number
- * @min: minimum number to guess
- * @max: maximum number to guess
- *
- * Return: square root of @n or -1
-*/
-int _sqrt_recursion_wrapper(int n, int min, int max)
+ * @n: number
+ * Return: int
+ */
+int _sqrt_recursion(int n)
 {
-	int guess, guess_squared;
+	int square = 2;
 
-	guess = (min + max) / 2;		/*get guess (g)*/
-	guess_squared = guess * guess;		/*square guess (sg)*/
-
-	if (guess_squared == n)			/*if squared guess == n return guess*/
-		return (guess);
-	else if (min == max)			/*this means n doesn't have a perfect square*/
+	if (n < 0)
 		return (-1);
-	else if (guess_squared < n)		/*overshoot g if sg is less than the n*/
-		return (_sqrt_recursion_wrapper(n, guess + 1, max));
-	else					/*undershoot g if sg is more than the n*/
-		return (_sqrt_recursion_wrapper(n, min, guess - 1));
+	else if (n == 0 || n == 1)
+		return (n);
+	return (is_sqrt(n, square));
 }
 
 /**
- * _sqrt_recursion - a function that returns the natural
- *                   square root of a number
+ * is_sqrt - function to check whether it's a natural square root or not
  *
- * @n: input number
- *
- * Return: square root
-*/
-int _sqrt_recursion(int n)
+ * @n: number
+ * @square: test number
+ * Return: int
+ */
+int is_sqrt(int n, int square)
 {
-	if (n == 1)		/*sqrt(1) == 1*/
-		return (1);
-	else if (n == 0)	/*sqrt(0) == 0*/
-		return (0);
-	else if (n < 0)		/*sqrt(-n) == -1 (NA)*/
+	if (square * square == n)
+		return (square);
+	else if (square * square < n)
+		return (is_sqrt(n, square + 1));
+	else if (square * square > n)
 		return (-1);
-	else
-		return (_sqrt_recursion_wrapper(n, 1, n));
+	return (-1);
 }
