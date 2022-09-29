4df2c8941950acd590718f244d9c9194e6c0576c
lolomari <isepribo@gmail.com>
2022-09-29 09:57:08 +0100

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -34,12 +34,7 @@ int _sqrt_recursion_wrapper(int n, int min, int max)
 */
 int _sqrt_recursion(int n)
 {
-	if (n == 1)		/*sqrt(1) == 1*/
-		return (1);
-	else if (n == 0)	/*sqrt(0) == 0*/
+	if (n == 0)
 		return (0);
-	else if (n < 0)		/*sqrt(-n) == -1 (NA)*/
-		return (-1);
-	else
-		return (_sqrt_recursion_wrapper(n, 1, n));
+	return (sqrt_check(1, n));
 }
